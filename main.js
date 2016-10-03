57f10f06f47f379332eef205a227a619e19062e9
Augustė Nausėdaitė <auguste.nausedaite@student.auc.nl>
2016-10-03 18:32:59 +0200

    some initial setups to the personal website

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 console.log("Hello world!")
 $( 'h1' ).fadeOut( )
 $( 'h1' ).fadeIn( )
+$(function() {
+	var copyText = ['first line',
+	'second line',
+	'third line']
+})
+
+$ ( '#profile' ).on('click', function() {
+	$(' #profile ').text('this is sometext about me')
+})

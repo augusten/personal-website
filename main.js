1dbe4e05cbb06b9616884491c4c5e3e1f0f2eea4
Augustė Nausėdaitė <auguste.nausedaite@student.auc.nl>
2016-10-04 00:12:28 +0200

    Unnecessary file

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-console.log("Hello world!")
-$( 'h1' ).fadeOut( )
-$( 'h1' ).fadeIn( )
-$(function() {
-	var copyText = ['first line',
-	'second line',
-	'third line']
+$(document).ready(function() {
+	$('p#hello').hide()
+	$('p#project-list').hide()
+	$('p#contact-info').hide()
 })
 
-$ ( '#projects' ).on('click', function() {
-	$(' #projects ').text('some projectsss')
+$('h2#about').click(function(){
+	$('p#hello').slideToggle('slow');
+})
+
+$('h2#project').click(function(){
+	$('p#project-list').slideToggle('slow');
+})
+
+$('h2#contact').click(function(){
+	$('p#contact-info').slideToggle('slow');
 })

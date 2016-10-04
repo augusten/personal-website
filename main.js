a86bfc6f65972b701525e89bd5b330f7f32c3991
Augustė Nausėdaitė <auguste.nausedaite@student.auc.nl>
2016-10-04 11:00:25 +0200

    changes made in order to add background color, and also style the content sliding out after clicking on the About, Project and Contact ids

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
-	$('p#hello').hide()
-	$('p#project-list').hide()
-	$('p#contact-info').hide()
+	$( 'p#hello' ).hide( )
+	$( '.project-list' ).hide( )
+	$( 'p#contact-info' ).hide( )
+	$( 'h1' ).fadeOut( )
+	$( 'h1' ).fadeIn( 'slow' )
 })
 
-$('h2#about').click(function(){
-	$('p#hello').slideToggle('slow');
+$( 'h2#about' ).click(function(){
+	$( 'p#hello' ).slideToggle( 'slow' );
 })
 
-$('h2#project').click(function(){
-	$('p#project-list').slideToggle('slow');
+$( 'h2#project' ).click(function( ){
+	$( '.project-list' ).slideToggle( 'slow' );
 })
 
-$('h2#contact').click(function(){
-	$('p#contact-info').slideToggle('slow');
+$( 'h2#contact' ).click(function( ){
+	$( 'p#contact-info' ).slideToggle( 'slow' );
 })
